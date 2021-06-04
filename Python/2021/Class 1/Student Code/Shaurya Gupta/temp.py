33783448148628bb5b26cfb5aae23783d2a59be8
shaurya-gupt <78970712+shaurya-gupt@users.noreply.github.com>
2021-06-04 15:47:29 +0530

    temp.py

---
+++ Python/2021/Class 1/Student Code/Shaurya Gupta/temp.py
@@ -0,0 +1,10 @@
+ def kill_me():
+    print("ded")
+    
+   
+helloDidThisWork = input("please tell me that this worked")
+if helloDidThisWork == "yes".lower():
+    print("WOOOP")
+else:
+  kill_me()
+  
