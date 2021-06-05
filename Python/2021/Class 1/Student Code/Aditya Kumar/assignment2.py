35fe95af1b4991ca4e9717b279d473a757b99b48
Pantherman1000 <69421660+Pantherman1000@users.noreply.github.com>
2021-06-05 17:33:52 +0530

    Create assignment2.py

---
+++ Python/2021/Class 1/Student Code/Aditya Kumar/assignment2.py
@@ -0,0 +1,3 @@
+firstShop = 15 * int(input("How many cups of tea did you buy from the first shop? "))
+secondShop = 30 * int(input("How many cups of tea did you buy from the second shop? "))
+print("Your total bill is", firstShop + secondShop, "Rupees.")
