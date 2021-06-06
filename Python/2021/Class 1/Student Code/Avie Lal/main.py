ada6c3cc3983f8ed7facffb9baec1e5d1526955b
ALAL7 <74096188+ALAL7@users.noreply.github.com>
2021-06-06 15:45:15 +0530

    Add files via upload

---
+++ Python/2021/Class 1/Student Code/Avie Lal/main.py
@@ -0,0 +1,8 @@
+Tea1 = input('Tea from shop one =\n')
+Tea1 = int(Tea1)
+Tea1 = Tea1*15
+T2 = input('Tea from shop 2 =\n')
+T2 = int(T2)
+T2 = T2*30
+Tea_total = Tea1 + T2;
+print("Total bill =",Tea_total," Rupees")
