7afa8611cabb66e83d0566f6d044da0d2525b21c
vinayadua <85301764+vinayadua@users.noreply.github.com>
2021-06-06 16:04:24 +0530

    Add files via upload

---
+++ Python/2021/Class 1/Student Code/Vinaya Dua/assignment2.py
@@ -0,0 +1,9 @@
+teashop1 = int(input("enter the number of cups bought from the first teashop:"))
+price1 = print(15*teashop1, "price for teashop1")
+teashop2 = int(input("enter the number of cups bought from the second teashop:"))
+price2 = print(30*teashop2, "price for teashop2")
+print( 15*teashop1 + 30*teashop2 , "is the total bill")
+
+
+
+
