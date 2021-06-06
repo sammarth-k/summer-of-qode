e1a399f24b284ed18d46ba2b79486c07deddc827
bhavik977 <85336124+bhavik977@users.noreply.github.com>
2021-06-06 15:25:21 +0530

    Create  bhaviksassignment.py

---
+++ Python/2021/Class 1/Student Code/Bhavik Sahni/bhaviksassignment.py
@@ -0,0 +1,10 @@
+assignment 1
+number1 = int(input("Enter your age: "))
+number2 = 100 
+result = number2 - number1
+print (result) 
+assignment 2
+number1 = int(input("first shop: "))
+number2 = int(input(" second shop : "))
+result =number1+number2
+print(result)
