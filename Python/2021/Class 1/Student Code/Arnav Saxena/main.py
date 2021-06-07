fb917e45d28de2ee4c99841d03f7858cddd4f68c
arnavsaxena667 <82904990+arnavsaxena667@users.noreply.github.com>
2021-06-07 11:31:46 +0530

    assignment

--- Python/2021/Class 1/Student Code/Arnav Saxena/main.py
+++ Python/2021/Class 1/Student Code/Arnav Saxena/main.py
@@ -2,8 +2,8 @@ age = int(input("what is your age? "));
 diff = 100-age
 print("years you have before you turn 100 years old is ", diff);
 
-shop1 = int(input("how much did u buy from the 1st shop "))
-shop2 = int(input("how much did u buy from shop 2 "))
+shop1 = int(input("how much did u buy from the 1st shop "));
+shop2 = int(input("how much did u buy from shop 2 "));
 tot1 = shop1*15
 tot2 = shop2*30
 total = tot1+tot2
