6c9c079fc67d217c72dfef4d778373f428fad726
shaurya-gupt <78970712+shaurya-gupt@users.noreply.github.com>
2021-06-08 16:56:02 +0530

    Create assignment3.py

---
+++ Python/2021/Class 2/Student Code/Shaurya Gupta/assignment3.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter the lowest number of the range: "))
+num2 = int(input("Enter the highest number of the range: "))
+
+num3 = int(input("check to see if this number is in the range"))
+if num3 >= num1 and num3 <= num2:
+    print("it is in the range")
+else:
+    print("it is not in the range")
