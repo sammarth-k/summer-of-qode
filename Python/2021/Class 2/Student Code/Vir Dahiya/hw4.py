2682f3f515fadb3a12034effafd2f93fb4677b82
VirDahiya <85215079+VirDahiya@users.noreply.github.com>
2021-06-08 16:42:12 +0530

    Add files via upload

---
+++ Python/2021/Class 2/Student Code/Vir Dahiya/hw4.py
@@ -0,0 +1,5 @@
+chocolate = int(input("How many choclates do you want: "))
+if chocolate > 10:
+  print("The store only has 10 choc0lates sorry!")
+else:
+  print("Thanks for buying!","Your total bill is","$",chocolate*15)  
