20950a64a9f52740a22feb0a912aed1584b86ea7
shaurya-gupt <78970712+shaurya-gupt@users.noreply.github.com>
2021-06-09 16:22:41 +0530

    Create ClassAssignment1.py

---
+++ Python/2021/Class 3/Student Code/Shaurya Gupta/ClassAssignment1.py
@@ -0,0 +1,15 @@
+total = 0
+for i in range(10):
+    number = int(input("enter a number: "))
+    total +=number
+
+print(total, "is all the numbers added")
+
+
+count = 0
+total2 = 0
+while count < 10:
+    user_input = int(input("Enter a number"))
+    total2 += user_input
+    count += 1
+print(total2)
