bebf46d8675e5473fd41ef4f66af2104d9d5d9c0
Odaboy09 <85216156+Odaboy09@users.noreply.github.com>
2021-06-09 14:32:07 +0530

    Add files via upload

---
+++ Python/2021/Class 2/Student Code/Uday Kapoor/main.py
@@ -0,0 +1,13 @@
+print ("Assignment 1")
+print ("odd or even?")
+import Assignment_1
+
+print ("Assignment 2")
+print ("how many chocolates would you like?")
+import Assignment_2
+
+print ("Final Assignment, Assignment 3")
+print ("we will figure out if your given number falls in our range")
+import Assignment_3
+
+
