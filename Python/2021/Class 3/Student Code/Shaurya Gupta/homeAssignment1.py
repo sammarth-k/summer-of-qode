628a7607172227153cc4d971ff6223ad46be1fb0
shaurya-gupt <78970712+shaurya-gupt@users.noreply.github.com>
2021-06-10 17:10:41 +0530

    Create homeAssignment1.py

---
+++ Python/2021/Class 3/Student Code/Shaurya Gupta/homeAssignment1.py
@@ -0,0 +1,7 @@
+number = 1
+while number <= 50 and number >= 0:
+    number = int(input("enter number: "))
+if number > 50:
+    print("the number is greater than 50 ")
+else:
+    print("the number is  less than 0")
