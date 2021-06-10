e020a47dacfdccf543b08c4319eb3c745de9f731
Sammarth Kumar <47685020+sammarth-k@users.noreply.github.com>
2021-06-10 08:08:42 +0530

    Delete Assignment_2.py

--- Assignment_2.py
+++
@@ -1,9 +0,0 @@
-x=10
-y=15
-wanted = int(input("enter number: "))
-if (wanted>x):
-  print("sorry we only have ten chocolates")
-else:
-  print ("Available, calculating bill... ")
-  Total = (wanted*15)
-  print ("Your bill is", (Total), "rupees")
