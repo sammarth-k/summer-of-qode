7d7313158cf02c5a70f50062732f8b6ed462a8a5
Sammarth Kumar <47685020+sammarth-k@users.noreply.github.com>
2021-06-10 08:09:00 +0530

    Delete Assignment_3.py

--- Assignment_3.py
+++
@@ -1,5 +0,0 @@
-num = int(input("enter number: "))
-if (num>0) and (num<11):
-  print ("your number falls in our range of 1 to 10")
-else:
-  print ("sorry your number does not fall in our range pls try again")
