d891c48ff4a3900555387fd73e14364768869f1c
VirDahiya <85215079+VirDahiya@users.noreply.github.com>
2021-06-10 16:12:48 +0530

    Add files via upload

---
+++ Python/2021/Class 3/Student Code/Vir Dahiya/hw3.py
@@ -0,0 +1,9 @@
+while True:
+  
+  num1 = int(input("Enter number: "))
+  
+  if(num1<50 or num1<0):
+    print("Nice!The number is in the range")
+    break
+  else:
+    print("Sorry pls write another number")
---
+++ Python/2021/Class 3/Student Code/Vir Dahiya/hw5.py
@@ -0,0 +1,4 @@
+for i in range(0,100,4):
+  print(i)
+
+
