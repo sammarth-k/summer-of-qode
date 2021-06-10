9918f3f5fd1eef5471edd2377b7d529e118972bd
hsanghi12 <82219638+hsanghi12@users.noreply.github.com>
2021-06-10 14:10:32 +0530

    Create Assignment 1.py

---
+++ Python/2021/Class 3/Student Code/Harshit Sanghi/Assignment 1.py
@@ -0,0 +1,9 @@
+while True:
+  
+  num1 = int(input("Enter number: "))
+  
+  if(num1>50 or num1<0):
+    print("The loop has ended. It is in range")
+    break
+  else:
+    print("The number is not in range. Please Write Again")
