2104d35fb325e4839eceeccfeaf62d1d34366f55
vinayadua <85301764+vinayadua@users.noreply.github.com>
2021-06-10 09:28:05 +0530

    Add files via upload

---
+++ Python/2021/Class 3/Student Code/Vinaya Dua/assignment6.py
@@ -0,0 +1,14 @@
+#Make a loop that keeps asking for a number from the user. 
+#The loop terminates only if the number inputted is greater than 50 or less than 0 (negative).
+
+
+number = int(input("enter your number:"))
+while number > 50 or number < 0 :
+    number = number + 1
+
+print("your number is not in range!")
+
+
+
+    
+
---
+++ Python/2021/Class 3/Student Code/Vinaya Dua/assignment7.py
@@ -0,0 +1,9 @@
+#Imagine you work for a firm with 5 employees.
+# Write a program which allows all 5 employees to input their name and print it.
+
+x = 1
+while x<6:
+    input("enter your name:")
+    x += 1
+
+    
